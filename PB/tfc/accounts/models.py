7dd848c37f8264cb5277b8c8dd9e46dda2ce42a4
Chloe Zhao <yichenesia@gmail.com>
2022-11-13 15:35:09 -0500

    Added initial accounts setup and packages

--- PB/tfc/accounts/models.py
+++ PB/tfc/accounts/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class TFCUser(AbstractUser):
+    email = models.EmailField(null=False)
+    username = models.CharField(null=False)
+    password = models.CharField(null=False)
+    first_name = models.CharField(max_length=200, null=False)
+    last_name = models.CharField(max_length=200, null=False)
+    phone_number = models.CharField(max_length=200, null=False)
+    avatar = models.ImageField()
+
+    def __str__(self):
+        return self.username
+
 
 # class User(models.Model):
 #     first_name = models.CharField(max_length=200, null=False)
