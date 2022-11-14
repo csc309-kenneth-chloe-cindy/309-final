c829f52b780a8b25a2e1be0ba8a2eed75a9191bb
Chloe Zhao <yichenesia@gmail.com>
2022-11-13 21:39:15 -0500

    Adding update user profile view

--- PB/tfc/accounts/models.py
+++ PB/tfc/accounts/models.py
@@ -5,13 +5,13 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class TFCUser(AbstractUser):
-    email = models.EmailField(null=False)
+    email = models.EmailField(null=True)
     username = models.CharField(max_length=200, null=False, unique=True)
     password = models.CharField(max_length=200, null=False)
-    first_name = models.CharField(max_length=200, null=False)
-    last_name = models.CharField(max_length=200, null=False)
-    phone_number = models.CharField(max_length=200, null=False)
-    avatar = models.ImageField(upload_to='user-images')
+    first_name = models.CharField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
+    phone_number = models.CharField(max_length=200, null=True)
+    avatar = models.ImageField(upload_to='user-images', null=True)
 
     def __str__(self):
         return self.username
--- PB/tfc/accounts/urls.py
+++ PB/tfc/accounts/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .views import CreateUserView
+from .views import CreateUserView, UpdateUserProfileView
 
 app_name = 'accounts'
 
 urlpatterns = [
+    # path('hello/', HelloView.as_view()),
     path('register/', CreateUserView.as_view()),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('<user_id>/update/', UpdateUserProfileView.as_view()),
 ]
+
+# ABOUT:  path('login/', ...)
+# The login flow uses the JWT built-in view to return an access token and a refresh token associated
+# with the user that is logging in. The frontned should send the username and password to the backend as
+# the payload.
+# the Frontend should pull the "access" token from the 'return' of its call to the API
+# and save that access token in memory until it expires.
