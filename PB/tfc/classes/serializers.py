9a4d55b6049a40f73e82f6c5bc656bb59769f201
cindychen1023 <110849989+cindychen1023@users.noreply.github.com>
2022-11-15 18:28:27 -0500

    Update serializers.py

--- PB/tfc/classes/serializers.py
+++ PB/tfc/classes/serializers.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 from .models import ClassOffering, TimeInterval, Keyword, ClassInstance, UserEnroll
 from studios.serializers import StudioSerializer
+import datetime
 
 class ClassOfferingSerializer(serializers.ModelSerializer):
     studio = StudioSerializer()
@@ -26,6 +27,23 @@ class KeywordSerializer(serializers.ModelSerializer):
 
 class ClassInstanceSerializer(serializers.ModelSerializer):
     class_offering = ClassOfferingSerializer()
+    
+    def create(self, validated_data):
+        class_offering_data = validated_data.pop("class_offering")
+        class_offering = ClassOffering.objects.create(**class_offering_data)
+        weekday = TimeInterval.objects.filter(class_offering=class_offering).day
+        date = datetime.date.today()
+        end_date = class_offering.end_recursion_date
+        if weekday - date.weekday() <= 0:
+            date = datetime.timedelta(weekday - date.weekday() + 7)
+        else:
+            date = datetime.timedelta(weekday - date.weekday())
+        list = []
+        while date <= end_date:
+            list = list.append(ClassInstance(date=date, capacity_count=0, class_offering=class_offering))
+            date = datetime.timedelta(7)
+        class_instance = ClassInstance.objects.bulk_create(list)
+        return class_instance      
 
     class Meta:
         model = ClassInstance
