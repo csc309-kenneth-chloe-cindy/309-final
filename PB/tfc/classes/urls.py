2d1b6a12c9b4d1388f4ea17079908c1789f64718
cindychen1023 <110849989+cindychen1023@users.noreply.github.com>
2022-11-15 18:29:20 -0500

    Update urls.py

--- PB/tfc/classes/urls.py
+++ PB/tfc/classes/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .views import CreateClassView, CancelAllClassView, CancelClassInstanceView, EditClassView
+from .views import CreateClassOfferingView, EditClassOfferingView, DeleteClassOfferingView, CreateTimeIntervalView, EditTimeIntervalView
+from .views import CreateKeywordView, EditKeywordView, CreateClassInstanceView, DeleteAllClassInstanceView, DeleteSingleClassInstanceView
 
 app_name = 'classes'
 
 urlpatterns = [
-    path('<int:studio_id>/new/', CreateClassView.as_view()),
-    path('<int:studio_id>/cancel/<int:class_id>/all/', CancelAllClassView.as_view()),
-    path('<int:studio_id>/edit/<int:class_id>/', EditClassView.as_view()),
-    path('<int:studio_id>/cancel/<int:class_id>/<int:class_instance_id>/', CancelClassInstanceView.as_view())
+    path('<studio_id>/new/', CreateClassOfferingView.as_view()),
+    path('<studio_id>/edit/', EditClassOfferingView.as_view()),
+    path('<studio_id>/delete/', DeleteClassOfferingView.as_view()),
+    path('<class_offering_id>/new/time/', CreateTimeIntervalView.as_view()),
+    path('<class_offering_id>/edit/time/', EditTimeIntervalView.as_view()),
+    path('<class_offering_id>/new/keyword/', CreateKeywordView.as_view()),
+    path('<class_offering_id>/edit/keyword/', EditKeywordView.as_view()),
+    path('<class_offering_id>/add/instances/', CreateClassInstanceView.as_view()),
+    path('<class_offering_id>/delete/all/', DeleteAllClassInstanceView.as_view()),
+    path('<class_offering_id>/delete/<class_instance_id>/', DeleteSingleClassInstanceView.as_view())
 ]
