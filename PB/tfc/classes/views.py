6a0958cd70f2f6f43a4c50ba72104a12ba93194f
cindychen1023 <110849989+cindychen1023@users.noreply.github.com>
2022-11-15 18:28:49 -0500

    Update views.py

--- PB/tfc/classes/views.py
+++ PB/tfc/classes/views.py
@@ -7,29 +7,83 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from classes.serializers import ClassOfferingSerializer, TimeIntervalSerializer, KeywordSerializer, ClassInstanceSerializer, UserEnrollSerializer
 from studios.models import Studio
+import pandas as pd
+import datetime
+from datetime import date, timedelta
+from .models import TimeInterval, Keyword
 
 # Create your views here.
-class CreateClassView(CreateAPIView):
-    permission_classes = [IsAdminUser]
+"""
+    CLASS_OFFERING
+"""
+class CreateClassOfferingView(CreateAPIView):
+    #permission_classes = [IsAdminUser]
     serializer_class = ClassOfferingSerializer
 
-class CancelAllClassView(RetrieveAPIView, DestroyAPIView):
-    permission_classes = [IsAdminUser]
+class EditClassOfferingView(RetrieveAPIView, UpdateAPIView):
     serializer_class = ClassOfferingSerializer
+    #permission_classes = [IsAdminUser]
 
     def get_object(self):
-        return get_object_or_404(ClassOffering, id=self.kwargs['class_id'])
+        return get_object_or_404(ClassOffering, id=self.kwargs['class_offering_id'])
 
-class CancelClassInstanceView(RetrieveAPIView, DestroyAPIView):
-    permission_classes = [IsAdminUser]
+class DeleteClassOfferingView(DestroyAPIView):
+    serializer_class = ClassOfferingSerializer
+    #permission_classes = [IsAdminUser]
+
+
+"""
+    TIME_INTERVAL
+"""
+
+class CreateTimeIntervalView(CreateAPIView):
+    #permission_classes = [IsAdminUser]
+    serializer_class = TimeIntervalSerializer
+
+class EditTimeIntervalView(RetrieveAPIView, UpdateAPIView):
+    serializer_class = TimeIntervalSerializer
+    #permission_classes = [IsAdminUser]
+
+    def get_query(self):
+        return TimeInterval.objects.filter(class_offering=self.kwargs['class_offering_id'])
+
+"""
+    KEY_WORD
+"""
+
+class CreateKeywordView(CreateAPIView):
+    #permission_classes = [IsAdminUser]
+    serializer_class = KeywordSerializer
+
+class EditKeywordView(RetrieveAPIView, UpdateAPIView):
+    serializer_class = KeywordSerializer
+    #permission_classes = [IsAdminUser]
+
+    def get_query(self):
+        return Keyword.objects.filter(studio=self.kwargs['studio_id'])
+
+"""
+    CLASS_INSTANCE
+"""
+
+class CreateClassInstanceView(CreateAPIView):
+    #permission_classes = [IsAdminUser]
     serializer_class = ClassInstanceSerializer
 
+class DeleteAllClassInstanceView(DestroyAPIView):
+    #permission_classes = [IsAdminUser]
+    serializer_class = ClassInstanceSerializer
+
+    def get_query(self):
+        list = ClassInstance.filter(class_offering=self.kwargs['class_offering_id'])
+        new_list = []
+        for class_instance in list:
+            if class_instance.date > datetime.date.today():
+                new_list = new_list + class_instance
+        return new_list
+
+class DeleteSingleClassInstanceView(DestroyAPIView):
+    #permission_classes = [IsAdminUser]
+    serializer_class = ClassInstanceSerializer
     def get_object(self):
         return get_object_or_404(ClassInstance, id=self.kwargs['class_instance_id'])
-
-class EditClassView(RetrieveAPIView, UpdateAPIView):
-    serializer_class = ClassOfferingSerializer
-    permission_classes = [IsAdminUser]
-
-    def get_object(self):
-        return get_object_or_404(ClassOffering, id=self.kwargs['class_id'])
